38a1a01c92f867702dd73a66ada1f8afd07bcd0c
Jason <jason.huang@messagexchange.com>
2015-04-13 13:25:32 +1000

    use large data set

--- query_runner.py
+++ query_runner.py
@@ -1,5 +1,18 @@
 import pickle
 
 query_pkl = open('query.pkl', 'rb')
-
 queries = pickle.load(query_pkl)
+
+qrel_pkl = open('qrel.pkl', 'rb')
+qrels = pickle.load(qrel_pkl)
+
+posting_pkl = open('posting.pkl', 'rb')
+posting = pickle.load(posting_pkl)
+
+idf_pkl = open('idf.pkl', 'rb')
+idf = pickle.load(idf_pkl)
+
+query_pkl.close()
+qrel_pkl.close()
+posting_pkl.close()
+idf_pkl.close()
--- tokenizer.py
+++ tokenizer.py
@@ -1,13 +1,17 @@
 # Create bag of words representation of documents
 from __future__ import division
 from math import log
+from math import sqrt
 from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist 
+import pickle
+
 tokenizer = RegexpTokenizer(r'\w+')
+
 lemmatizer = WordNetLemmatizer()
 stemmer = PorterStemmer()
 stops = stopwords.words('english')
@@ -22,7 +26,7 @@ def lowercase(word):
   return word.lower()
 
 proj_root = '/home/jason/ir_engine/'
-document_root = proj_root + 'small'
+document_root = proj_root + 'blogs'
 
 # Use tokenizer to remove puntuations
 files = PlaintextCorpusReader(document_root, '.*\.txt')
@@ -38,6 +42,8 @@ for file in files.fileids():
   words = filter(stopping, words)
   words = map(stemming, words)
   words = FreqDist(words)
+  if words.values():
+      file_length_normalization = sqrt(reduce(lambda x, y: x+y*y, words.values()))
   for word, freq in words.iteritems():
     if idf.has_key(word):
       idf[word] += 1
@@ -46,6 +52,15 @@ for file in files.fileids():
 
     if not posting.has_key(word):
       posting[word] = {}
-    posting[word][file] = freq
+    posting[word][file] = freq/file_length_normalization
 
 idf = { term: log(N/dft) for term, dft in idf.iteritems() }
+
+
+posting_pickle = open('posting.pkl', 'wb')
+pickle.dump(posting, posting_pickle)
+posting_pickle.close()
+
+idf_pickle = open('idf.pkl', 'wb')
+pickle.dump(idf, idf_pickle)
+idf_pickle.close()
